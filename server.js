4880064dec5074b48caa116866c20b5de057d3bb
timabs <timur.abd.nyc@gmail.com>
2023-09-30 02:31:15 -0400

    CORS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mime = require("mime");
+const cors = require("cors");
 require("dotenv").config();
 
 const nodemailer = require("nodemailer");
@@ -12,25 +13,13 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("uhhh lemme get uhhh form");
 });
-// app.get("/main.js", (req, res) => {
-//   res.setHeader("Content-Type", "text/javascript");
-//   res.sendFile(__dirname + "/main.js");
-// });
-// app.get("/contact.css", (req, res) => {
-
-//   res.setHeader("Content-Type", "text/css");
-//   res.sendFile(__dirname + "/contact.css");
-// });
-// app.get("/home.css", (req, res) => {
-
-//   res.setHeader("Content-Type", "text/css");
-//   res.sendFile(__dirname + "/home.css");
-// });
-// app.get("/homeMobile.css", (req, res) => {
-
-//   res.setHeader("Content-Type", "text/css");
-//   res.sendFile(__dirname + "/homeMobile.css");
-// });
+app.use(
+  cors({
+    origin: "http://localhost:53440", // Replace with the actual origin of your frontend
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
 app.post("/form", (req, res) => {
   const transporter = nodemailer.createTransport({
