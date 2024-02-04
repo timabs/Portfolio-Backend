20b595aa46840df784a3e3a61402a30ef0380785
timabs <timur.abd.nyc@gmail.com>
2024-02-04 16:59:27 -0500

    Testing

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 app.use(
   cors({
-    origin: ["https://morselane.com", "http://localhost:3000"],
+    origin: ["https://morselane.com", "http://localhost:5173"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
